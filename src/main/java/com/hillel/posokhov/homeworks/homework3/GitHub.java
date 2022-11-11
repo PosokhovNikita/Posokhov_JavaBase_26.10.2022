52ebc55a9071115e1bf8c797a2f5f7c1c89a295d
Posokhov Nikita <itposokhovnikita@gmail.com>
2022-11-11 15:21:06 +0200

    all files

--- src/main/java/com/hillel/posokhov/homeworks/homework3/GitHub.java
+++ src/main/java/com/hillel/posokhov/homeworks/homework3/GitHub.java
@@ -2,6 +2,14 @@ package com.hillel.posokhov.homeworks.homework3;
 
 public class GitHub {
     public static void main(String[] args) {
+    int a = 100;
+    int b = 50;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
 
+        int result = a / b + 18;
+        System.out.println("Result =" + result);
     }
 }
