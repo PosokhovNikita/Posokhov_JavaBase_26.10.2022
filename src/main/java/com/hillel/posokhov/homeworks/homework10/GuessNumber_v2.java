9fdcafd15274657e098001c15e387819aad381d5
Posokhov Nikita <itposokhovnikita@gmail.com>
2022-12-14 19:02:52 +0200

    homework remake

--- src/main/java/com/hillel/posokhov/homeworks/homework10/GuessNumber_v2.java
+++ src/main/java/com/hillel/posokhov/homeworks/homework10/GuessNumber_v2.java
@@ -1,31 +1,36 @@
 package com.hillel.posokhov.homeworks.homework10;
 
+import java.util.Scanner;
+
 public class GuessNumber_v2 {
     public static void main(String[] args) {
+        int num = (int) (Math.random() * 11);
+        int guess = -1;
+        int counter = 0;
+        int attempt = 3;
+        System.out.println(num);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Let's play a game, you need to guess the number from 0 to 10 that the computer randomized! ");
+        System.out.println("Enter number for 0 to 10: ");
+        System.out.println("You have a " + (attempt)+ " attempts!");
 
-        int num = 0;
-
-        for (int i = 1; i < 4; i++) {
-            System.out.println("Внешний цикл i=" + i);
-
-            for (int j = 1; j < 4; j++) {
-                System.out.print("\tВнутренний цикл j=" + j);
-                System.out.println("\t\tВсего num=" + (++num));
-            }
+        while (guess != num) {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Wrong data, try again.");
+                scanner.nextLine();}
+            guess = scanner.nextInt();
+            counter++;
+            if (counter == attempt){
+                System.out.println("YOU LOSE!!!");
+                break;}
+            if (guess == num) {
+                System.out.println("YOU WIN!!!");
+                break;
+            } else if (guess < num)
+                System.out.println("Your number is LESS than predicted! Try Again!");
+            else
+                System.out.println("Your number is GREATER than predicted! Try Again!");
         }
+        System.out.println("Attempts: " + counter);
     }
 }
-//for ( инициализация ; проверочное-выражение ; итерация ) {
-//операторы-для-выполнения-на-каждой-итерации ; }
-//Скобки после ключевого слова for должны содержать три управляющих выражения, которые и определяют действие цикла.
-
-//• Инициализация — присваивает начальное значение переменной- счетчику, который будет
-// подсчитывать число итераций цикла. Переменная для этих целей может быть объявлена прямо здесь,
-// и обычно это самая простая целочисленная переменная с именем i.
-
-//• Проверочное выражение — данное выражение оценивается в начале каждой итерации цикла на предмет
-// логического значения true. Когда оценка возвращает значение true, итерация продолжается, а при возвращении
-// значения false цикл немедленно прекращает свою работу, не завершая текущую итерацию.
-
-//• Итерация — изменяет текущее значение переменной-счетчика, храня в себе общее число итераций, сделанных циклом.
-// Обычно здесь используется выражение i++ для увеличения либо i-- для уменьшения значения счетчика
