44b451b30f65d05fd2243c41630a9edb1f2bc610
Posokhov Nikita <itposokhovnikita@gmail.com>
2022-12-22 21:29:56 +0200

    homework

--- src/main/java/com/hillel/posokhov/homeworks/homework11/Matrix_v2.java
+++ src/main/java/com/hillel/posokhov/homeworks/homework11/Matrix_v2.java
@@ -4,49 +4,55 @@ import java.util.Scanner;
 
 public class Matrix_v2 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a size of array M: ");
-        while (!scanner.hasNextInt()) {
-            System.out.println("Wrong data, try again.");
-            scanner.nextLine();}
-        int M = scanner.nextInt();
-        System.out.println("Enter a size of array N: ");
-        while (!scanner.hasNextInt()) {
-            System.out.println("Wrong data, try again.");
-            scanner.nextLine();}
-        int N = scanner.nextInt();
+        int M = getNumber(scanner);
+        int N = getNumber(scanner);
+
         int[][] array = new int[M][N];
         int[][] array2= new int[N][M];
 
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    array[i][j] = M * i + j;
-                    array [i][j]= N * i + j;
-                }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 101);
             }
-            System.out.println("First Array:");
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    System.out.print(array[i][j] + "\t");
-                }
-                System.out.println();
+        }
+        System.out.println("First array:");
+        print(array);
+
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = array[j][i];
             }
-            for (int i = 0; i < M; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    int temp = array[i][j];
-                    array[i][j] = array[j][i];
-                    array[j][i] = temp;
-                }
-            }
-            System.out.println();
-            System.out.println("Second Array:");
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    System.out.print(array2[i][j] + "\t");
-                }
-                System.out.println();
-            }
+        }
+        System.out.println("Second array:");
+        print(array2);
     }
 
+    static void print(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    static int getNumber(Scanner scanner) {
+        int temp = 0;
+        System.out.println("Please enter array length");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                temp = scanner.nextInt();
+                if (temp < 0) {
+                    System.out.println("Please enter positive number");
+                } else {
+                    break;
+                }
+                scanner.nextLine();
+            } else {
+                System.out.println("Wrong data, try again");
+                scanner.nextLine();
+            }
+        }
+        return temp;
+    }
 }
